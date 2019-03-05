70635013412f1427ab9c0f8a16939aad223521a4
Grant Glidewell <grantglidewell@Grants-MacBook-Pro.local>
2019-03-05 14:01:20 -0800

    🎉

---
+++ README.md
@@ -0,0 +1,62 @@
+# gatsby-source-randomuser
+
+This source plugin makes randuser.me data available via GraphQL queries in Gatsby
+
+## installation
+
+```
+# Install the plugin
+yarn add gatsby-source-randomuser
+```
+
+in `gatsby-config.js`
+
+```
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-randomuser',
+      options: {
+        // options
+      }
+    }
+  ]
+};
+```
+
+## Configuration options
+
+[Randomuser.me options](https://randomuser.me/documentation#howto) can be used with this plugin
+
+example configuration:
+
+```
+options: {
+  results: 25,
+  seed: "foobar",
+  inc: "name,email",
+  nat: "fr",
+  gender: "female",
+  password: "upper,1-8",
+}
+```
+
+## Query random users
+
+```
+{
+  allRandomUser {
+    edges {
+      node {
+        id
+        name {
+          title
+          first
+          last
+        }
+        email
+      }
+    }
+  }
+}
+```
---
+++ gatsby-node.js
@@ -0,0 +1,39 @@
+const fetch = require("node-fetch")
+const queryString = require("query-string")
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  configOptions
+) => {
+  const { createNode } = actions
+
+  const processUser = user => {
+    const nodeId = createNodeId(`randomUser-${user.email}`)
+    const nodeContent = JSON.stringify(user)
+    const nodeData = Object.assign({}, user, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `randomUser`,
+        content: nodeContent,
+        contentDigest: createContentDigest(user),
+      },
+    })
+    return nodeData
+  }
+
+  configOptions.format = "json"
+
+  return fetch(
+    `https://randomuser.me/api/?${queryString.stringify(configOptions)}`
+  )
+    .then(data => data.json())
+    .then(json => json.results)
+    .then(users =>
+      // modify users for createNode with node IDS
+      users.map(user => {
+        createNode(processUser(user))
+      })
+    )
+}
